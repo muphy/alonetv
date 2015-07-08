fdd92b398c62df1b43048df3b8880350ba3a9fbf
Jongam <shinemuphy@gmail.com>
2015-07-08 16:26:52 +0900

    aa

--- routes/users.js
+++ routes/users.js
@@ -25,17 +25,23 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
   var newUser = new User(req.body);
 
-  var query = { 'provider': newUser.provider,'id':newUser.id };
+  var query = { 'provider': newUser.provider, 'id': newUser.id };
   // req.newData.username = req.user.username;
   User.findOne(query, function (err, user) {
     if (err) return res.send(500, { error: err });
-    newUser.save(function(err) {
-      if(!err) {
-        return res.json(200, req.body);
-      } else {
-        return res.json(500, { error: err });
-      }
-    });
+    if (!user) {
+      var newUser = new User(req.body);
+      newUser.save(function (err) {
+        if (!err) {
+          return res.json(200, req.body);
+        } else {
+          return res.json(500, { error: err });
+        }
+      });
+    } else {
+      return res.json(200, user);
+    }
+    
   });
 });
 
