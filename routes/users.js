04c959ab4cd7963aba294585ed41964a62d447f0
Jongam <shinemuphy@gmail.com>
2015-07-08 15:55:40 +0900

    mongooses findOneAndUpdate function emit wierd errors. => err: After applying the update to the document

--- routes/users.js
+++ routes/users.js
@@ -7,25 +7,37 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+/**
+ * findOneAndUpdate emit wierd errors. => err: "After applying the update to the document
+ */
+// router.post('/', function (req, res, next) {
+//   var newUser = new User(req.body);
+
+//   var query = { 'provider': newUser.provider,'id':newUser.id };
+//   // req.newData.username = req.user.username;
+//   User.findOneAndUpdate(query, newUser, { upsert: true }, function (err, doc) {
+//     if (err) return res.send(500, { error: err });
+//     return res.json(200, req.body);
+//   });
+// });
+
 /* create new user. */
 router.post('/', function (req, res, next) {
   var newUser = new User(req.body);
-  // newUser.save(function (err) {
-  //   if (err) {
-  //     console.log('error:' + err);
-  //   } else {
-  //     console.log('success');
-  //   }
-  // });
-  // res.json(200, req.body);
-
 
   var query = { 'provider': newUser.provider,'id':newUser.id };
   // req.newData.username = req.user.username;
-  User.findOneAndUpdate(query, newUser, { upsert: true }, function (err, doc) {
+  User.findOne(query, function (err, user) {
     if (err) return res.send(500, { error: err });
-    return res.json(200, req.body);
+    newUser.save(function(err) {
+      if(!err) {
+        return res.json(200, req.body);
+      } else {
+        return res.json(500, { error: err });
+      }
+    });
   });
 });
+
 
 module.exports = router;
